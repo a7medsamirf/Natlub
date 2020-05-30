112623936cae6f78176add4d8484e4ffd1623114
a7medsamirf <ahmed.samir.farid@gmail.com>
2020-05-30 22:01:54 +0200

    V 1.0

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,6 +36,7 @@ $(document).ready(function () {
     cursorborder: "5px solid #e2a841",
     cursorborderradius: 0,
     horizrailenabled: false,
+    zindex:9999,
     cursorfixedheight: 250
   });
 });
@@ -157,7 +158,7 @@ $(document).ready(function ($) {
         }).on('circle-animation-progress', function (event, progress, stepValue) {
           $(this).find('.progress-value').text((stepValue * 100).toFixed(0) + "k");
         }).stop();
-        $(this).find('.Four').circleProgress({
+        $(this).find('.Fourth').circleProgress({
           value: percent / 100,
           size: 140,
           thickness: 10,
@@ -239,7 +240,6 @@ $('.customers').slick({
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
-        arrows: false,
       }
     },
     {
@@ -247,7 +247,6 @@ $('.customers').slick({
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
       }
     },
 
@@ -264,166 +263,3 @@ $('.customers').slick({
 });
 
 
-var timezone = [
-  {
-    "value": "Dateline Standard Time",
-    "abbr": "DST",
-    "offset": -12,
-    "isdst": false,
-    "text": "(UTC-12:00) International Date Line West",
-    "utc": [
-      "Etc/GMT+12"
-    ]
-  },
-  {
-    "value": "UTC-11",
-    "abbr": "U",
-    "offset": -11,
-    "isdst": false,
-    "text": "(UTC-11:00) Coordinated Universal Time-11",
-    "utc": [
-      "Etc/GMT+11",
-      "Pacific/Midway",
-      "Pacific/Niue",
-      "Pacific/Pago_Pago"
-    ]
-  },
-  {
-    "value": "Eastern Standard Time",
-    "abbr": "EDT",
-    "offset": -4,
-    "isdst": true,
-    "text": "(UTC-05:00) Eastern Time (US & Canada)",
-    "utc": [
-      "America/Detroit",
-      "America/Havana",
-      "America/Indiana/Petersburg",
-      "America/Indiana/Vincennes",
-      "America/Indiana/Winamac",
-      "America/Iqaluit",
-      "America/Kentucky/Monticello",
-      "America/Louisville",
-      "America/Montreal",
-      "America/Nassau",
-      "America/New_York",
-      "America/Nipigon",
-      "America/Pangnirtung",
-      "America/Port-au-Prince",
-      "America/Thunder_Bay",
-      "America/Toronto",
-      "EST5EDT"
-    ]
-  },
-
-  {
-    "value": "Arab Standard Time",
-    "abbr": "AST",
-    "offset": 3,
-    "isdst": false,
-    "text": "(UTC+03:00) Kuwait, Riyadh",
-    "utc": [
-      "Asia/Aden",
-      "Asia/Bahrain",
-      "Asia/Kuwait",
-      "Asia/Qatar",
-      "Asia/Riyadh"
-    ]
-  },
-];
-
-
-// plugin start from here
-(function($){
-
-    $.fn.worldClock = function(){
-        clockInit(this);
-    }
-
-    var clockInit = function(obj){
-        obj.each(function(){
-            var t = getTimezoneArray( $(this).attr("data-timezone") );
-            runClock( $(this), t );
-        });
-    }
-
-    var runClock = function( el, timezone ){
-        setInterval(function(){
-            var clockArray = getClock(timezone);
-            setClockHTML( el, clockArray );
-        }, 1000);
-    }
-
-    var getDegree = function(c, m){
-        return c/m*360;
-    }
-
-    var setClockHTML = function( clock, clockArray ){
-
-        var secondsDegree   = getDegree(clockArray[2], 60);
-        var minutesDegree   = getDegree(clockArray[1], 60);
-        var hoursDegree     = getDegree(clockArray[0], 12);
-
-        if ( clockArray[0] >= 12 ){
-            clock.find(".apm").html("PM");
-        } else{
-            clock.find(".apm").html("AM");
-        }
-
-        clock.find(".seconds").css({
-            "-webkit-transform": "rotateZ("+secondsDegree+"deg)",
-            "-ms-transform": "rotateZ("+secondsDegree+"deg)",
-            "transform": "rotateZ("+secondsDegree+"deg)"
-        });
-
-        minutesDegree = minutesDegree+clockArray[2]/60*6;
-
-        clock.find(".minutes").css({
-            "-webkit-transform": "rotateZ("+minutesDegree+"deg)",
-            "-ms-transform": "rotateZ("+minutesDegree+"deg)",
-            "transform": "rotateZ("+minutesDegree+"deg)"
-        });
-
-        hoursDegree = hoursDegree+clockArray[1]/60*30;
-
-        clock.find(".hours").css({
-            "-webkit-transform": "rotateZ("+hoursDegree+"deg)",
-            "-ms-transform": "rotateZ("+hoursDegree+"deg)",
-            "transform": "rotateZ("+hoursDegree+"deg)"
-        });
-    }
-
-    var getClock = function( timezone ){
-
-        var d = new Date();
-        if ( timezone != undefined ){
-            var utcTimeOffset = parseFloat(timezone.offset)*60*60*1000;
-
-            var utcTime = d.getTime() + (d.getTimezoneOffset()*60*1000); // perfect utc timestamp
-            d = new Date(utcTime + utcTimeOffset);
-        }
-
-
-        var getHours    = d.getHours();
-        var getMinutes  = d.getMinutes();
-        var getSeconds  = d.getSeconds();
-
-        return [getHours, getMinutes, getSeconds];
-    }
-
-    var getTimezoneArray = function( tz ){
-        if ( tz == undefined ){
-            return tz;
-        }
-        var timezoneArray = undefined;
-        $.each(timezone, function(){
-            if ( this['utc'].toString().indexOf(tz) != -1 ){
-                timezoneArray = this;
-                return true;
-            }
-        });
-        return timezoneArray;
-    }
-
-})(jQuery);
-
-$(".clock").worldClock();
